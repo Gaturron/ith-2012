c1272e8ad20aae7756efdafebc0f6ee66d90815c
fernando.bugni <fernando.bugni@8d8bbb2e-ef09-8412-40fd-0bdc7f30f325>
2012-10-09 19:58:25 +0000

    sigo con parametros

--- trunk/tp2/main.py
+++ trunk/tp2/main.py
@@ -14,6 +14,15 @@ def checkDiphones(str):
             if not(str[i] in vocal):
                 print 'Mal input: '+str[0:i+1]
 
+def getDiphones(str):
+    assert len(str) > 1, 'str debe ser mas largo'    
+    
+    res = []
+    res.append('-'+str[0])
+    for i in range(1, len(str)):
+        res.append(str[i-1]+str[i])
+    res.append(str[len(str)-1]+'-')       
+
 if len(sys.argv) < 2:
     print 'Mal pasaje de parametros: debe pasar el codigo en ascii de lo que quiere sintetizar'
 else:
@@ -21,4 +30,5 @@ else:
     print 'Parametro ingresado: '+str
 
     checkDiphones(str)
+    print getDiphones(str)
     
