aba97a4e3ae231b8aa5dd7ed847f8034a63628c8
fernando.bugni <fernando.bugni@8d8bbb2e-ef09-8412-40fd-0bdc7f30f325>
2012-10-14 07:03:22 +0000

    base de difonos

--- trunk/tp2/main.py
+++ trunk/tp2/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import commands
+
 vocal = ['a', 'A']
 consonant = ['k', 'l', 'm', 'p', 's']
 
@@ -26,6 +28,30 @@ def getDiphones(string):
     res.append(string[len(string)-1]+'-')   
     return res    
 
+def makePraatScript(diphones, string):
+    #se genera el script de praat
+    praatReads = ''
+    praatPlus  = ''
+    script     = ''
+    p1         = '-' + string + '-'
+    #diphones  = [p1[i:i+2] for i in range(len(p1) - 1)]
+    soundNames = 'difono'
+    soundCount = 0
+    for each_diphone in diphones:
+        fName   = each_diphone + '.wav'
+        praatReads += 'Read from file... difonos/base1/' + fName + '\n'
+        praatReads += 'Rename... ' + soundNames + str(soundCount) + '\n'    #se renombra cada difono para que praat seleccione bien los objetos.
+        praatPlus  += 'plus Sound ' + soundNames + str(soundCount) + '\n'
+        soundCount += 1
+    
+    script += praatReads + praatPlus
+    script += 'Concatenate recoverably\n'
+    script += 'select Sound chain\n'
+    script += 'Write to WAV file... ' + string + '.wav'
+    
+    return script
+
+#Empieza el script
 if len(sys.argv) < 2:
     print 'Mal pasaje de parametros: debe pasar el codigo en ascii de lo que quiere sintetizar'
 else:
@@ -35,11 +61,21 @@ else:
     if not checkDiphones(string):
         print 'Mal los difonos ingresados'
     else:
+        print 'Sintetizando: '+string
         diphones = getDiphones(string)
 
-        for i in range(0, len(diphones)):
-            if i == 0:
-                print 'select Sound '+diphones[0]
-            else:
-                print 'plus Sound '+diphones[i]
-        print 'Concatenate recoverably'
+        scriptToRun = makePraatScript(diphones, string)
+        
+        print 'Generando script para praat...',
+        scriptFile = open(string + '.praat', 'w')
+        scriptFile.writelines(scriptToRun)
+        scriptFile.close()
+        print 'Ok'
+
+        print 'Ejecutando praat...',
+        status = commands.getstatusoutput('./praat ' + string + '.praat')
+        
+        if status[0] != 0:
+            print 'Error al tratar de ejecutar el script, mensaje: ', status[1]
+        else:
+            print 'Ok'
