e55b9917cfec0ed969d8a20d7c3e8d32760790b0
fernando.bugni <fernando.bugni@8d8bbb2e-ef09-8412-40fd-0bdc7f30f325>
2012-10-15 20:13:28 +0000

    main arreglado

--- tp2/main.py
+++ tp2/main.py
@@ -6,76 +6,83 @@ import commands
 vocal = ['a', 'A']
 consonant = ['k', 'l', 'm', 'p', 's']
 
-def checkDiphones(string):
-    for i in range(0, len(string)):
+diphones_dir = "difonos/base1/"
+
+def checkDiphones(input):
+    for i in range(0, len(input)):
         if i % 2 == 0:
-            if not(string[i] in consonant):
-                print 'Mal input: '+string[0:i+1]
+            if not(input[i] in consonant):
+                print 'Mal input: '+input[0:i+1]
                 return False
         else:
-            if not(string[i] in vocal):
-                print 'Mal input: '+string[0:i+1]
+            if not(input[i] in vocal):
+                print 'Mal input: '+input[0:i+1]
                 return False
     return True
 
-def getDiphones(string):
-    assert len(string) > 1, 'str debe ser mas largo'    
+def getDiphones(input):
+    assert len(input) > 1, 'str debe ser mas largo'    
     
     res = []
-    res.append('-'+string[0])
-    for i in range(1, len(string)):
-        res.append(string[i-1]+string[i])
-    res.append(string[len(string)-1]+'-')   
+    res.append('-'+input[0])
+    for i in range(1, len(input)):
+        res.append(input[i-1]+input[i])
+    res.append(input[len(input)-1]+'-')   
     return res    
 
-def makePraatScript(diphones, string):
-    #se genera el script de praat
-    praatReads = ''
-    praatPlus  = ''
-    script     = ''
-    p1         = '-' + string + '-'
-    #diphones  = [p1[i:i+2] for i in range(len(p1) - 1)]
-    soundNames = 'difono'
+    #input2    = '-' + input + '-'
+    #return [input2[i:i+2] for i in range(len(input2) - 1)]
+
+def makePraatScript(input):
+    diphones = getDiphones(input)
+
+    loadWavs = ''
+    concatWavs  = ''
     soundCount = 0
     for each_diphone in diphones:
-        fName   = each_diphone + '.wav'
-        praatReads += 'Read from file... difonos/base1/' + fName + '\n'
-        praatReads += 'Rename... ' + soundNames + str(soundCount) + '\n'    #se renombra cada difono para que praat seleccione bien los objetos.
-        praatPlus  += 'plus Sound ' + soundNames + str(soundCount) + '\n'
+        loadWavs += 'Read from file... '+ diphones_dir + each_diphone + '.wav' + '\n'
+        loadWavs += 'Rename... difono' + str(soundCount) + '\n'  
+        concatWavs += 'plus Sound difono' + str(soundCount) + '\n'
         soundCount += 1
     
-    script += praatReads + praatPlus
+    script = loadWavs 
+    script += concatWavs
     script += 'Concatenate recoverably\n'
     script += 'select Sound chain\n'
-    script += 'Write to WAV file... ' + string + '.wav'
+    script += 'Write to WAV file... ' + input + '.wav'
     
     return script
+
+def makePraatFile(input, script):
+    praatFile = open(input + '.praat', 'w')
+    praatFile.writelines(script)
+    praatFile.close()
 
 #Empieza el script
 if len(sys.argv) < 2:
     print 'Mal pasaje de parametros: debe pasar el codigo en ascii de lo que quiere sintetizar'
 else:
-    string = sys.argv[1]
-    print 'Parametro ingresado: '+string
+    input = sys.argv[1]
 
-    if not checkDiphones(string):
-        print 'Mal los difonos ingresados'
+    if input == "-help":
+        print "Abirir el archivo LEEME"
     else:
-        print 'Sintetizando: '+string
-        diphones = getDiphones(string)
+        print 'Parametro ingresado: '+input
 
-        scriptToRun = makePraatScript(diphones, string)
-        
-        print 'Generando script para praat...',
-        scriptFile = open(string + '.praat', 'w')
-        scriptFile.writelines(scriptToRun)
-        scriptFile.close()
-        print 'Ok'
-
-        print 'Ejecutando praat...',
-        status = commands.getstatusoutput('./praat ' + string + '.praat')
-        
-        if status[0] != 0:
-            print 'Error al tratar de ejecutar el script, mensaje: ', status[1]
+        if not checkDiphones(input):
+            print 'Mal los difonos ingresados'
         else:
-            print 'Ok'
+            print 'Sintetizando: '+input
+
+            script = makePraatScript(input)
+            
+            print 'Generando script para praat...'
+            makePraatFile(input, script)
+
+            print 'Ejecutando praat...'
+            status = commands.getstatusoutput('./praat ' + input + '.praat')
+            
+            if status[0] != 0:
+                print 'Error al tratar de ejecutar el script, mensaje: ', status[1]
+            else:
+                print 'Ok'
